4fa6799b668bb0d83d830b14957c366fd8cb5fcc
Gabriela Caballero <g.caballero@uniandes.edu.co>
2021-11-29 18:10:45 -0500

    Taller

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,9 @@
 import React from "react";
+import {FormattedDate} from 'react-intl';
+import NumberFormat from 'react-number-format';
+
+
+
 
 const Job = (props) => {
   return (
@@ -6,9 +11,31 @@ const Job = (props) => {
       <th scope="row">{props.offer.id}</th>
       <td>{props.offer.name}</td>
       <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
+      <td>
+   <NumberFormat 
+     value={props.offer.salary} 
+     displayType={'text'}
+     suffix=" million"
+     />  
+  </td>
       <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <td>
+  <FormattedDate
+    value={new Date(props.offer.date)}
+    year='numeric'
+    month='long'
+    day='numeric'
+    weekday='long'
+    
+  />
+</td>
+      <td>
+   <NumberFormat 
+     value={props.offer.visits} 
+     displayType={'text'}
+     thousandSeparator={true}
+     />  
+  </td>
     </tr>
   );
 };
--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Job from "./job";
+import {FormattedMessage, FormattedHTMLMessage} from 'react-intl';
+
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +12,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      visits: "134",
     },
     {
       id: "0002",
@@ -18,6 +21,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      visits: "1250",
     },
     {
       id: "0003",
@@ -26,6 +30,7 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      visits: "1780",
     },
   ]);
 
@@ -35,11 +40,24 @@ const JobsList = () => {
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Publication date"/>
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Visits"/>
+            </th>
           </tr>
         </thead>
         <tbody>
